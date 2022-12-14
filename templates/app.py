bc35469b657aacf468c764ba40e7d24f351bfd84
jenningstopps <jenningsT@live.com>
2022-12-14 14:53:32 -0800

    First Commit

---
+++ templates/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+
+# Selects the page for which a function is to be defined. Right now there will only be one page in your website.
+
+@app.route('/')
+
+def hello():
+    print("Hello World")
+    env_var = os.environ['PATH']
+    print("PATH: ", env_var)
+
+    return "<h1>Hello World!</h1>" \
+          "\nThis is my introduction to Flask!" \
+         "\nI can write a lot of things on this page.\nLet's get started!"
+
+    #return render_template('app.html')
+
+# The above function returns the HTML code to be displayed on the page
+
+
+
+if __name__ == '__main__':
+
+   app.run()
